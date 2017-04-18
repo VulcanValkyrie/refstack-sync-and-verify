76edb67379b33c6c130f3f2b0ccc889aafef75ca
VulcanValkyrie <guinm@cat.pdx.edu>
2017-04-17 22:08:27 -0700

    can now specify server & no longer directly access db

--- update-rs-db.py
+++ update-rs-db.py
@@ -38,7 +38,7 @@ def fixLink(link):
     return newurls
 
 
-def linkInDb(link, cursor):
+def linkInDb(link):
     testId = link.split('/')[-1]
     matches = requests.get(
         'https://refstack.openstack.org/v1/results/' + testId)
@@ -48,79 +48,55 @@ def linkInDb(link, cursor):
     return True
 
 
-def getTestId(productName, cursor):
-    # i think this may be a thing that actually really needs to be querying
-    # the local db, but i am not sure
-    cursor.execute("SELECT id FROM product WHERE name = '%s'" % (productName))
-    productId = cursor.fetchone()
-    if productId is None:
-        print ("No product associated with the name " +
-               productName + " was found")
-        return None
-    else:
-        productId = productId[0]
-    cursor.execute(
-        "SELECT id from product_version WHERE product_id = '%s'" % (productId))
-    productVersionId = cursor.fetchone()
-    if productVersionId is None:
-        #print("No product version associated with the given name was found")
-        return None
-    else:
-        productVersionId = productVersionId[0]
-    cursor.execute(
-        "SELECT id from test WHERE product_version_id = '%s'" % (productVersionId))
-    testId = cursor.fetchone()
-    if testId is None:
-        #print("No test associated with the given name was found")
-        return None
-    else:
-        testId = testId[0]
-        return testId
+def getTestId(link):
+    testId = link.split('/')[-1]
+    return testId
 
 
-def processLink(line, cursor):
+def processLink(line, server):
     if not line[0]:  # or not line[1]:
         return False, None
-    #print(line[0] + " is being processed")
     links = []
+    update = False
     refstack_link = line[9]
     apiLinks = fixLink(refstack_link)
     if apiLinks is None:
-        print(
-            "No links found for the cloud associated with the company " + line[0] + ".")
+        print("No links found for the cloud associated with the company " +
+              line[0] + " on the provided spreadsheet.")
         return False, None
     for link in apiLinks:
         resultsStatus = linksChk(link)
         if not resultsStatus:
-            print("Result is broken or does not exist")
-            return False, None
-        linkMatch = linkInDb(link, cursor)
+            #print("Result is broken or does not exist")
+            update = False
+        linkMatch = linkInDb(link)
         if not linkMatch:
-            return False, None
-    # now that we have verified that the link exists
-    # get the related testId
-    testId = getTestId(line[1], cursor)
-    # next, we will update the our local refstack db
-    if testId is None:
-        return False, None
-    print("updating shared")
-    resp = requests.post(
-        'https://refstack.openstack.org/v1/results/testId/meta/shared', data='true')
-    guideline = line[4]
-    if guideline and guideline != "" and guideline != " ":
-        print("updating guideline")
+            #print("there is no such result in your db")
+            update = False
+        testId = getTestId(link)
+        if testId is None:
+            update = False
+        #print("updating shared")
         resp = requests.post(
-            'https://refstack.openstack.org/v1/results/testId/meta/guideline', data=guideline)
-    targetProgram = line[5]
-    if targetProgram and targetProgram != "" and targetProgram != " ":
-        print("updating target program")
-        resp = requests.post(
-            'https://refstack.openstack.org/v1/results/testId/meta/target', data=targetProgram)
-    # now return the related test ID
-    if testId is None:
-        return False, None
-    else:
-        return True, testId
+            "http://" + server + '/v1/results/' + testId + '/meta/shared', data='true')
+        guideline = line[4]
+        if guideline and guideline != "" and guideline != " ":
+            #print("updating guideline")
+            resp = requests.post(
+                "http://" + server + '/v1/results/' + testId + '/meta/guideline', data=guideline)
+        targetProgram = line[5]
+        if targetProgram and targetProgram != "" and targetProgram != " ":
+            #print("updating target program")
+            resp = requests.post(
+                "http://" + server + '/v1/results/' + testId + '/meta/target', data=targetProgram)
+        if testId is None or not update or update != True:
+            update = False
+        else:
+            return True, testId
+    if update == False:
+        print("There is no link associated with the company " +
+              line[0] + " in your database.")
+    return update, testId
 
 
 def splitLine(line):
@@ -130,29 +106,34 @@ def splitLine(line):
 
 
 def main():
-    # connect to the internal refstack db
-    db = pymysql.connect("localhost", < user > , < password > , "refstack")
-    cursor = db.cursor()
-    # connect to the sourcefile
     parser = argparse.ArgumentParser(
         "Update the internal RefStack db using a csv file")
-    parser.add_argument('--file', "-f", metavar='f', type=str,
+    parser.add_argument("--file", "-f", metavar='f', type=str,
                         action="store", dest="filename", default="toadd.csv")
+    parser.add_argument("--server", "-s", metavar='s', type=str, action="store",
+                        dest="server", default="https://refstack.openstack.org/v1/results/")
     result = parser.parse_args()
     if result.filename is None or not result.filename:
         infile = "toadd.csv"
     else:
         infile = result.filename
+    if result.server is None or not result.server:
+        server = "https://refstack.openstack.org"
+    else:
+        server = result.server
     with open(infile) as f:
         for line in f:
-            # check if the data in this line can be verified
             entry = []
             entry = splitLine(line)
-            verify, testId = processLink(entry, cursor)
+            verify, testId = processLink(entry, server)
             if verify:
-                #"PUT /v1/results/testId request body:{verification_status: 1}"
-                print("marking product data as verified")
+                print("marking the product data associated with the company " +
+                      entry[0] + " as verified")
                 resp = requests.put(
-                    'https://refstack.openstack.org/v1/results/testId/', data={'verification_status': 1})
+                    "http://" + server + '/v1/results/' + testId, data={'verification_status': 1})
             else:
-                print("This product cannot be verified.\n")
+                print("the entry associated with the company " +
+                      entry[0] + " cannot be verified.\n")
+
+
+main()
