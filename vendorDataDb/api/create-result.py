389d15dd74641fb6f177da8cd7df9e068fe31262
VulcanValkyrie <guinm@cat.pdx.edu>
2017-04-23 21:43:37 -0700

    add create result functionality, moved utility functions into create.py

---
+++ vendorDataDb/api/create-result.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3.5
+import pymysql
+import argparse
+import sys
+import create
+
+
+def process_flags(cursor, results):
+    if not results.result - link or results.result - link is " ":
+        print("No RefStack result link provided. Exiting.")
+        sys.exit()
+    else:
+        if create.linkChk(results.result - link):
+            resultlink = results.result - link
+            _id = resultlink.split(",")[-1]
+        else:
+            print("Cannot add broken result link. Exiting.")
+            sys.exit()
+    if not results.product - name or results.product - name is " ":
+        print("No product name provided. Exiting.")
+        sys.exit()
+    else:
+        productname = results.result - link
+        productId = create.getProductId(cursor, name)
+    if not results.ticket - link or results.ticket - link is " ":
+        tikID = "NULL"
+    else:
+        tik_link = results.ticket - link
+        tikId = tik_link.split("/")[-1]
+    if not results.guideline or results.guideline is " ":
+        guideline = "NULL"
+    else:
+        guideline = results.guideline
+    return _id, resultlink, productId, tikId, guideline
+
+
+def create_result(cursor, db, _id, result, refstack, productId, tikId, guideline):
+    if not dupChk("result", "id", _id):
+        query = "INSERT INTO result(id, tik_id, guideline, _product_id_, refstack) VALUES('%s', '%s', '%s', '%d', '%s')" % (
+            _id, tikId, guideline, productId, refstack)
+        cursor.execute(query)
+        db.commit()
+    else:
+        print("The result associated with the ID " + _id + "already exists")
+
+
+def main():
+    db, cursor, parser = create.connect)
+    parser.add_argument("-r", "--result-link", type = str,
+                        action = "store", required = True, help = "Refstack result link")
+    parser.add_argument("-p", "--product-name", type = str, action = "store",
+                        required = True, help = "Product associated with this set of test results")
+    parser.add_argument("-t", "--ticket-link", type = str,
+                        action = "store", help = "Associated ticket link")
+    parser.add_argument("-g", "--guideline", type = str,
+                        action = "store", help = "Associated Defcore guideline")
+    results=parser.parse_args()
+    _id, refstack, productId, tikId, guideline=process_flags(cursor, results)
+    create_result(cursor, db, _id, result, refstack,
+                  productId, tikId, guideline)
+    db.close()
--- vendorDataDb/api/create.py
+++ vendorDataDb/api/create.py
@@ -1,28 +1,71 @@
 #!/usr/bin/python3.5
 import pymysql
 import argparse
+import urllib.error
+import urllib.request
 
 
 def dupChk(table, keytype, keyval, cursor):
-    query = "SELECT COUNT(*) FROM %s WHERE %s = '%s'"%(table, keytype, keyval)              
+    query = "SELECT COUNT(*) FROM %s WHERE %s = '%s'" % (table,
+                                                         keytype, keyval)
     cursor.execute(query)
-    rowcount = cursor.rowcount                                                              
-    if rowcount != 0:                                                                       
-        return True                                                                         
+    rowcount = cursor.rowcount
+    if rowcount != 0:
+        return True
     else:
-        return False                                                                        
+        return False
+
 
 def process_flags(results):
-    if results.table is None:                                                               
-        table = "N/A"                                                                       
+    if results.table is None:
+        table = "N/A"
     else:
         table = results.table
-    return table                                                           
+    return table
+
+
+def linkChk(link):
+    if link is None or not link:
+        return False
+    try:
+        if " " in link:
+            return False
+        response = urllib.request.urlopen(link)
+        if response.geturl() == link:
+            return True
+    except urllib.error.HTTPError as err:
+        return False
+
 
 def connect():
-    db = pymysql.connect("<vendordata server db>", "<user>", "<password>", "vendorData")
+    db = pymysql.connect("<MySQL db server>", "<username>",
+                         "<password>", "vendorData")
     cursor = db.cursor()
-    parser = argparse.ArgumentParser()               
+    parser = argparse.ArgumentParser()
     return db, cursor, parser
 
 
+def getCompanyId(cursor, company):
+    if company is None or company is " ":
+        sys.exit()
+    query = "SELECT id FROM company where name = '%s'" % (company)
+    companyId = cursor.execute(query)
+    if companyId is None or companyId is 0 or companyId is "":
+        print("No matching organization found. Exiting.")
+        sys.exit()
+    else:
+        companyId = companyId[0]
+    return companyId
+
+
+def getProductId(cursor, name):
+    if name is None or name is " ":
+        sys.exit()
+    query = "SELECT id FROM company where name = '%s'" % (name)
+    productId = cursor.execute(query)
+    if productId is None or productId is 0:
+        print("No matching product Id found. Exiting.")
+        sys.exit()
+    else:
+        productId = productId[0]
+    return productId
